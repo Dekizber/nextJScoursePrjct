3035e4c3445ce0f8bcfcbb1b8928b44938706c7c
dekizber <dekizber@gmail.com>
2025-01-21 17:34:51 +0200

    add conditional styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './globals.css';
-import StatusLabel from './components/status-label';
 
 export default function RootLayout({
   children,
@@ -10,7 +9,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
-      <StatusLabel>Active</StatusLabel>
     </html>
   );
 }
